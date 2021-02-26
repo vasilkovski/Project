0ed2ada53de74d2c7e50e2b3bc61a9b4b28b2f5d
Nikola Vasilkovski <61252479+vasilkovski@users.noreply.github.com>
2021-02-26 10:31:42 +0100

    Delete PHP vanila OOP basics 2 directory

--- PHP vanila OOP basics 2/interfaces.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Market;
-
-interface Buyable
-{
-
-    public function total();
-}
--- PHP vanila OOP basics 2/kiwi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Market;
-class Kiwi extends Products {
-    protected $kilos = true;
-    protected $price = 670;
-
-}
